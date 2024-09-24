61d250abfda68330b964c04d1cc73f6d3c245772
Lê Hà Phan <128871268+nhimtjofc@users.noreply.github.com>
2024-09-24 22:43:29 +0700

    Update User.java

--- src/main/java/com/prj/entities/User.java
+++ src/main/java/com/prj/entities/User.java
@@ -1,45 +1,31 @@
 package com.prj.entities;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter
 @Entity
 @Table(name = "users")
 public class User {
 
-    @Column(name = "Username")
-    private String username;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
 
-    @Column(name = "Password")
-    private String password;
-
-    @Column(name = "Email")
     private String email;
-
-    @Column(name = "Full name")
     private String fullName;
-
-    @Column(name = "Phone number")
     private String phone;
-
-    @Column(name = "Address")
     private String address;
 
-    public User getUsername() {
-        return null;
-    }
+    private String username; // Thêm thuộc tính username
+    private String password; // Thêm thuộc tính password
 
 }
+
 
