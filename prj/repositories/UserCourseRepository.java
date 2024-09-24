04e72f2d099361fc95067ca6f31bda6187f8d024
DoTraNgocChau <22520147@gm.uit.edu.vn>
2024-09-24 23:52:01 +0700

    Revert "patching errors"
    This reverts commit 13690193b392b463fed520a298e88796635eae19.

--- prj/repositories/UserCourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prj.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.prj.entities.UserCourse;
-
-public interface UserCourseRepository extends JpaRepository<UserCourse, Long>{
-
-}
