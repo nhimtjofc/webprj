2ef0c7bfb866cf48c834c8df8e8773327c491165
DoTraNgocChau <161134289+Hedgehog2411@users.noreply.github.com>
2024-09-24 08:52:21 +0700

    fix lombok usage

--- src/main/java/com/prj/entities/Role.java
+++ src/main/java/com/prj/entities/Role.java
@@ -5,11 +5,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;;
 
-@Setter
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "Role")
 public class Role {
