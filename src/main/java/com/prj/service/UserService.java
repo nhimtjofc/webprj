8dd6b6117056d81a1c0957f496d1f36e43f1ca39
Lê Hà Phan <128871268+nhimtjofc@users.noreply.github.com>
2024-09-24 22:47:13 +0700

    Update UserService.java

--- src/main/java/com/prj/service/UserService.java
+++ src/main/java/com/prj/service/UserService.java
@@ -2,7 +2,6 @@ package com.prj.service;
 
 import com.prj.entities.User;
 import com.prj.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,8 +10,11 @@ import java.util.Optional;
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     // Lấy tất cả người dùng
     public List<User> getAllUsers() {
