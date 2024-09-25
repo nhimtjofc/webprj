1dfc0cb1ac03f66e32c9c178caa68c598d48eb3e
Lê Hà Phan <128871268+nhimtjofc@users.noreply.github.com>
2024-09-25 20:54:34 +0700

    Delete src directory

--- src/main/java/com/prj/repositories/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prj.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.prj.entities.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Long>{
-
-}
